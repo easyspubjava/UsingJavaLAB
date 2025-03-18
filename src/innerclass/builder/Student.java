9404c2909d6f8587ecccc5723ff16b214d78dc15
easyspubjava <eunjong.park@gmail.com>
2025-03-18 20:38:14 +0900

    java examples

---
+++ src/innerclass/builder/Student.java
@@ -0,0 +1,28 @@
+package innerclass.builder;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private int grade;
+    private String major;
+    private String phoneNumber;
+
+
+    public void showInfo(){
+        System.out.println("학번 :" + id);
+        System.out.println("이름 :" + name);
+        System.out.println("전공   :" + major);
+        System.out.println("전화번호 :" + phoneNumber);
+
+    }
+
+    public static void main(String[] args) {
+        Student student = new Builder(1, "John Doe")
+                .grade(12)
+                .major("Computer Science")
+                .phoneNumber("123-456-7890")
+                .build();
+        student.showInfo();
+    }
+}
